fce48533ed53bad678d916b2b6b70beec7fddd97
joshlacey <williamjoshualacey@gmail.com>
2017-07-06 14:56:24 -0400

    done

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,19 +1,56 @@
+require 'pry'
 def get_first_name_of_season_winner(data, season)
-  # code here
+    contestant = ""
+    data[season].each do |hash|
+        if hash["status"] == "Winner"
+            contestant= hash["name"].split.shift
+            end
+        end
+    contestant
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+    contestant = ""
+    data.each do |season, list|
+        list.each do |hash|
+            if hash["occupation"] == occupation
+                contestant = hash["name"]
+            end
+        end
+    end
+    contestant
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+    hometown_count = 0
+    data.each do |season, list|
+        list.each do |hash|
+            if hash["hometown"] == hometown
+                hometown_count += 1
+            end
+        end
+    end
+    hometown_count
 end
 
 def get_occupation(data, hometown)
-  # code here
+  contestant = ""
+  data.each do |season, list|
+      list.find do |hash|
+          if hash["hometown"] == hometown
+              contestant = hash["occupation"]
+            end
+          end
+      end
+  contestant
 end
 
 def get_average_age_for_season(data, season)
-  # code here
+    ages_total = 0
+  data[season].each do |hash|
+      ages_total += hash["age"].to_i
+      end
+  ave_age = (ages_total / data[season].length.to_f)
+  ave_age.round
 end
+
